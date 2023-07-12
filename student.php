798465d5ab3019ce4b47fbe98c4ab9b3f58ed0ad
Archit Gupta <gupta.archit01@gmail.com>
2023-07-12 13:26:10 +0530

    Update student.php

--- student.php
+++ student.php
@@ -5,8 +5,8 @@ session_start();
 
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-  header("location: index.php ");
-  exit;
+    header("location: index.php ");
+    exit;
 }
 
 echo $_SESSION['username'];
@@ -23,7 +23,8 @@ $prof = mysqli_fetch_assoc($data);
 
 <!doctype html>
 <html lang="ar" dir="rtl">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,55 +33,118 @@ $prof = mysqli_fetch_assoc($data);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" integrity="sha384-PJsj/BTMqILvmcej7ulplguok8ag4xFTPryRq8xevL7eBYSmpXKcbNVuy+P0RMgq" crossorigin="anonymous">
 
     <title></title>
-  </head>
-  <body>
-  <div class="container1">
-    <div class="profile">
-      <!-- <div class="image"></div> -->
 
-      <?php
-      if ($_SESSION['role'] == 'admin') {
+    <style>
+    /* $body: #09000b;
+    $nunito: "Nunito", sans-serif;
+    $dark: #0a0a0a; */
 
-        echo '<a href="./admin.php">Go to Admin Page</a>';
-      }
-      ?>
-      <h2><?php echo $_SESSION['username'] ?></h2>
-      <h5> <?php echo $prof['email'] ?> </h5>
-      <h5><?php echo $prof['mobile_no'] ?></h5>
-      <h5> <?php echo $prof['role'] ?></h5>
-      <h5> <?php echo $prof['uid'] ?></h5>
-      <h5> Courses: -
-        <?php
-        // session_start();
+    body {
+      background-color: red;
+      font-family: "Nunito", sans-serif;
+    }
 
-        $uid1 = $_SESSION['uid'];
-        $use = $_SESSION['username'];
-        $sql1 = "Select distinct(course_code) from courses where creator = '$use'";
-        $p_data = mysqli_query($conn, $sql1);
-        // $proj = mysqli_fetch_assoc($p_data);
-        while ($prod = mysqli_fetch_assoc($p_data)) {
-          // echo $prod['title'] ; 
-          // echo '<a href="./course.php?name=$prod['creator']">' . $prod['title'] . '</a>';
-          echo '<a href="./course.php?course=' . $prod['course_code'] . '">' . $prod['course_code'] . '</a>';
+    .container1 {
+      padding: 100px 0;
 
-          echo ", ";
+    }
+
+    .profile {
+      max-width: 300px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      padding: 30px;
+      border-radius: 10px;
+      position: relative;
+      display: grid;
+      justify-content: start;
+    }
+
+    h2 {
+      color: #0a0a0a;
+    }
+
+    p {
+      color: rgba(#0a0a0a, 0.8)
+    }
+
+    .image {
+      width: 75px;
+      height: 75px;
+      background: red;
+      border-radius: 50%;
+      margin: 0 auto;
+      position: absolute;
+      right: 15px;
+      top: 15px;
+      transform-origin: bottom left;
+      box-shadow: 0 3px 15px rgba(#0a0a0a, 0.1);
+      transition: all 0.3s ease-in-out;
+
+      /* background-image: url("https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80"); */
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+
+    /* &:hover {
+        .image {
+        transform: scale(1.5);
+        border-radius: 10px;
         }
-        ?>
-      </h5>
+    } */
+  </style>
+</head>
 
-      <!-- <h5>email</h5> -->
-      <button class="edit" id="<?php echo $prof['uid']; ?>" href="/edit">Edit</button>
+<body>
+    <div class="container1">
+        <div class="profile">
+            <!-- <div class="image"></div> -->
 
+            <?php
+            if ($_SESSION['role'] == 'admin') {
+
+                echo '<a href="./admin.php">Go to Admin Page</a>';
+            }
+            ?>
+            <h2><?php echo $_SESSION['username'] ?></h2>
+            <h5> <?php echo $prof['email'] ?> </h5>
+            <h5><?php echo $prof['mobile_no'] ?></h5>
+            <h5> <?php echo $prof['role'] ?></h5>
+            <h5> <?php echo $prof['uid'] ?></h5>
+            <h5> Courses: -
+                <?php
+                // session_start();
+
+                $uid1 = $_SESSION['uid'];
+                $use = $_SESSION['username'];
+                $sql1 = "Select distinct(course_code) from courses where creator = '$use'";
+                $p_data = mysqli_query($conn, $sql1);
+                // $proj = mysqli_fetch_assoc($p_data);
+                while ($prod = mysqli_fetch_assoc($p_data)) {
+                    // echo $prod['title'] ; 
+                    // echo '<a href="./course.php?name=$prod['creator']">' . $prod['title'] . '</a>';
+                    echo '<a href="./course.php?course=' . $prod['course_code'] . '">' . $prod['course_code'] . '</a>';
+
+                    echo ", ";
+                }
+                ?>
+            </h5>
+
+            <!-- <h5>email</h5> -->
+            <button class="edit" id="<?php echo $prof['uid']; ?>" href="/edit">Edit</button>
+
+        </div>
     </div>
-  </div>
 
 
-    
+
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-    
-  </body>
+
+</body>
+
 </html>
