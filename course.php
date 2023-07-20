29bc02e413f7cbf068de805f606d58661528f941
Archit Gupta <gupta.archit01@gmail.com>
2023-07-20 10:36:57 +0530

    Update course.php

--- course.php
+++ course.php
@@ -26,85 +26,7 @@ $user = $_SESSION['uid'];
 echo $user;
 
 
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-  echo 'yes';
-  if (isset($_POST['attendance'])) {
-    
-    echo 'yes1';
-    $attendance = $_POST['attendance'];
-    $cid = $_POST['cid'];
-    $reason = $_POST['reason'];
 
-
-    $query1 = "Select * from attendance where student_id = '$user' AND cid = '$cid' ";
-    $result1 = mysqli_query($conn, $query1);
-
-    
-    
-
-    if (mysqli_num_rows($result1) == 0) {
-      // The query result is empty
-      echo 'Query result is empty.';
-      $query2 = "INSERT INTO `attendance` (`cid`, `student_id`, `attended`, `reason`) VALUES ('$cid', '$user', '$attendance', '$reason')";
-      $result2 = mysqli_query($conn, $query2);
-      // $query3 = "Select * from attendance where student_id = '$user' AND cid = '$cid' ";     
-      // $result3 = mysqli_query($conn, $query3); 
-      if($attendance!=1) {     
-        $query5 = "SELECT * FROM courses WHERE cid = '$cid'";
-        $result5 = mysqli_query($conn, $query5);
-        $core = mysqli_fetch_assoc($result5);
-
-        echo $core['down_pdf'];
-
-       
-        $file = "files/material/".$core['down_pdf']; 
-        // $file = "check.txt"; 
-
-        echo $file;
-
-        header('Content-Description: File Transfer');
-
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-
-        echo basename($file);
-
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file));
-        // header('Content-Length: ' . filesize($file));
-
-        echo "  ";
-
-        echo filesize($file);
-        readfile($file);
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
-        exit; 
-      }
-
-
-     
-
-
-    } 
-  } elseif(isset($_POST['feedback'])) {
-    echo 'yes2';
-    $feedback = $_POST['feedback'];
-    $cid = $_POST['cid'];
-
-    $query3 = "INSERT INTO `feedback` (`uid`, `cid`, `feedback`) VALUES ('$user', '$cid', '$feedback')";
-    $result3 = mysqli_query($conn, $query3);
-
-
-    
-
-
-
-  }
-}
 ?>
   <!-- Modal 2 -->
   <div class="modal fade" tabindex="-1" id="attend1" tabindex="-1" aria-labelledby="attend1Label" aria-hidden="true">
@@ -305,12 +227,10 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     edits = document.getElementsByClassName('edit');
     Array.from(edits).forEach((element) => {
       element.addEventListener("click", (e) => {
+
         console.log("edit");
-
         var ex = e.target.id;
-
         $('#editModal').modal('toggle');
-
         attends = document.getElementsByClassName('attend');
         Array.from(attends).forEach((element) => {
           element.addEventListener("click", (e) => {
