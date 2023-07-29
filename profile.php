c9518b8b9592df8a3009bb1ea130e3ddab7002f9
Archit Gupta <gupta.archit01@gmail.com>
2023-07-29 11:45:22 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -255,6 +255,9 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
 
 
+
+
+
   <!-- Button trigger modal -->
   <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Launch demo modal
@@ -296,10 +299,37 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
 
   <!-- -------------------------profile-------------------------------- -->
+  <div class="container1 row justify-content-center ">
+    
+  <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary-emphasis">Welcome</strong>
+          <h3 class="mb-0"><?php echo $_SESSION['username'] ?></h3>
+          <div class="mb-1 text-body-secondary"><?php echo date('Y-m-d') ;?></div>
+          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <br>
+          <h6>Email:- <?php echo $prof['email'] ?> </h6>
+      <h6>Mobile No.:- <?php echo $prof['mobile_no'] ?></h6>
+      <h6>Role:- <?php echo $prof['role'] ?></h6>
+      <h6>User Id:-  <?php echo $prof['uid'] ?></h6>
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+            Continue reading
+            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+    </div>
 
-  <div class="container1 ">
-    <div class="profile border rounded ">
+  
       <!-- <div class="image"></div> -->
+      <div class="container1 row justify-content-center ">
+
+      <div class="profile border rounded ">
 
       <?php
       if ($_SESSION['role'] == 'admin') {
