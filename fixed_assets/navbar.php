d4e0b3a0097a76e577af409950aba63fbcb0be56
Archit Gupta <gupta.archit01@gmail.com>
2023-07-30 18:15:24 +0530

    Added

--- fixed_assets/navbar.php
+++ fixed_assets/navbar.php
@@ -293,7 +293,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="../Php_learning/discussions.php">
                 <svg class="bi"><use xlink:href="#discuss"/></svg>
                 Discussions
               </a>
