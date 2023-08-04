e70fdcd90957af6ec2c3a0561c564d3f458f6e80
Archit Gupta <gupta.archit01@gmail.com>
2023-08-04 12:15:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 
 
 
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "Hello world";?></title>
-</head>
-<body>
-
-<h1>Hello</h1>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+  <h1>Hello</h1>
 <a href="./login_guest.php">guest</a><br>
 
 <a href="./login_admin.php">admin</a><br>
 <a href="./login_faculty.php">faculty</a><br>
 <a href="./register.php">register</a><br>
 <a href="./login_student.php">Student</a><br>
-    
-</body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
 </html>
+
+
+
 
 
 
