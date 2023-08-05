4d7e6f86c63c77d23abef7f2a6014a912b44e280
Archit Gupta <gupta.archit01@gmail.com>
2023-08-05 18:58:43 +0530

    Update quiz_marks.php

--- quiz_marks.php
+++ quiz_marks.php
@@ -1,105 +1,4 @@
-<!-- <div class="container mb-5">
-    <div class="row">      
-        <div class="col-12">
-            <p class="fw-bold mt-5">2. Complete the following sentences:Alice couldn't _______ the humilation any longer and stormed out of the room red as a bed</p>
-            <div>
-                <div class="row">
-                    <div class="col-md-6"> <input type="radio" name="box" id="five"> <label for="five" class="box fifth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject">is</span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="six"> <label for="six" class="box sixth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> was </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="seven"> <label for="seven" class="box seveth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> will </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="eight"> <label for="eight" class="box eighth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> None of the above </span> </div>
-                        </label> </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            <p class="fw-bold mt-5">2. Complete the following sentences:Alice couldn't _______ the humilation any longer and stormed out of the room red as a bed</p>
-            <div>
-                <div class="row">
-                    <div class="col-md-6"> <input type="radio" name="box" id="five"> <label for="five" class="box fifth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject">is</span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="six"> <label for="six" class="box sixth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> was </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="seven"> <label for="seven" class="box seveth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> will </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="eight"> <label for="eight" class="box eighth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> None of the above </span> </div>
-                        </label> </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            <p class="fw-bold mt-5">2. Complete the following sentences:Alice couldn't _______ the humilation any longer and stormed out of the room red as a bed</p>
-            <div>
-                <div class="row">
-                    <div class="col-md-6"> <input type="radio" name="box" id="five"> <label for="five" class="box fifth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject">is</span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="six"> <label for="six" class="box sixth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> was </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="seven"> <label for="seven" class="box seveth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> will </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="eight"> <label for="eight" class="box eighth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> None of the above </span> </div>
-                        </label> </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            <p class="fw-bold mt-5">2. Complete the following sentences:Alice couldn't _______ the humilation any longer and stormed out of the room red as a bed</p>
-            <div>
-                <div class="row">
-                    <div class="col-md-6"> <input type="radio" name="box" id="five"> <label for="five" class="box fifth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject">is</span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="six"> <label for="six" class="box sixth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> was </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="seven"> <label for="seven" class="box seveth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> will </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="eight"> <label for="eight" class="box eighth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> None of the above </span> </div>
-                        </label> </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            <p class="fw-bold mt-5">2. Complete the following sentences:Alice couldn't _______ the humilation any longer and stormed out of the room red as a bed</p>
-            <div>
-                <div class="row">
-                    <div class="col-md-6"> <input type="radio" name="box" id="five"> <label for="five" class="box fifth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject">is</span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="six"> <label for="six" class="box sixth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> was </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="seven"> <label for="seven" class="box seveth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> will </span> </div>
-                        </label> </div>
-                    <div class="col-md-6"> <input type="radio" name="box" id="eight"> <label for="eight" class="box eighth w-100">
-                            <div class="course"> <span class="circle"></span> <span class="subject"> None of the above </span> </div>
-                        </label> </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12">
-            <div class="d-flex justify-content-center"> <button class="btn btn-primary px-4 py-2 fw-bold"> continue</button> </div>
-        </div>
-    </div>
-</div> -->
+
 
 
 <?php
@@ -115,12 +14,9 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     // $sql1 = "UPDATE `evaluation` SET `marks` = 1  WHERE `evaluation`.`eid` = 2";
     // $p_data = mysqli_query($conn, $sql1);
    
-    if (isset($_POST['quiz_end'])) {
+    if (isset($_POST['quiz_end'])) {      
 
-       
-
-        // echo "Quiz";
-       
+        // echo "Quiz";    
         
         // Process the selected option as needed
         $marks = 0;
@@ -139,9 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             // Process each key-value pair as neede
             if($key!='quiz_end'){
             if($postData[$key]==$correctArray[$key]){
-                $marks+=1;
-
-            
+                $marks+=1;            
             }}}
 
         echo "your marks:-". $marks;
